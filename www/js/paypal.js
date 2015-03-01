26eebc9f3817bfa1f327be5dd0cd5021224d56d1
Frank Pan <frankpzh@gmail.com>
2015-02-28 17:00:31 -0800

    Update and rename www/js to www/js/paypal.js

---
+++ www/js/paypal.js
@@ -0,0 +1,10 @@
+function getAccessToken() {
+  $clientId = "Add3LX8D--vVVxlK5uYNecoLPHQs5p6CRkuWGIMQ0CFKkUc1Msskh-8WFds0SP0eV75QFx9aMhHpxxlm"
+  $secret = "EJha7cYA-_t_Od--8lHe9XLiSf1bOqreT7_1OyfmTdWFOw55LiBRttIBv57a7wjGwOgTPcXrae0lpZqR"
+  return $token
+}
+
+function payment($payer) {
+  $accessToken = getAccessToken()
+  return $redirectUrl
+}
