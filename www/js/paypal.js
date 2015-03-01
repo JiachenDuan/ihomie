250c4ca19bdc5f30f3e1ff4c8e62470b8b57f395
Frank Pan <frankpzh@gmail.com>
2015-02-28 17:32:44 -0800

    Update paypal.js

--- www/js/paypal.js
+++ www/js/paypal.js
@@ -1,10 +1,14 @@
-function getAccessToken() {
-  $clientId = "Add3LX8D--vVVxlK5uYNecoLPHQs5p6CRkuWGIMQ0CFKkUc1Msskh-8WFds0SP0eV75QFx9aMhHpxxlm"
-  $secret = "EJha7cYA-_t_Od--8lHe9XLiSf1bOqreT7_1OyfmTdWFOw55LiBRttIBv57a7wjGwOgTPcXrae0lpZqR"
-  return $token
+var paypalApiUrl = "https://api.sandbox.paypal.com"
+var clientId = "Add3LX8D--vVVxlK5uYNecoLPHQs5p6CRkuWGIMQ0CFKkUc1Msskh-8WFds0SP0eV75QFx9aMhHpxxlm"
+var secret = "EJha7cYA-_t_Od--8lHe9XLiSf1bOqreT7_1OyfmTdWFOw55LiBRttIBv57a7wjGwOgTPcXrae0lpZqR"
+
+var getAccessToken = function(onError) {
+  var paypalToken = $resource(paypalApiUrl + "/v1/oauth2/token")
+  paypalToken.get().$promise
+  return token
 }
 
-function payment($payer) {
-  $accessToken = getAccessToken()
-  return $redirectUrl
+var payment = function(payer, onError) {
+  var accessTokenPromise = getAccessToken(paypalApiUrl, onError)
+  return redirectUrl
 }
